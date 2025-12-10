77d31bb115a17a5b644ddb22a6f0ec4eaef05e4f
bonzoholda <bhumibeka@gmail.com>
2025-12-10 23:37:11 +0700

    Update SwapWidgetPancake.jsx

--- src/components/Trading/SwapWidgetPancake.jsx
+++ src/components/Trading/SwapWidgetPancake.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const USDT_ADDRESS = "0xd5210074786CfBE75b66FEC5D72Ae79020514afD";
-const SMOS_ADDRESS = "0x75a683b1bb0bCfC3A0Da4b67f1D1Eb6D82A4Ad6c";
+const SMOS_ADDRESS = "0x2080C3184d1A421D68AEb0901e75e8fdF09D71C1";
 
 const PANCAKESWAP_URL = 
   `https://pancakeswap.finance/swap?chain=bsc-testnet` +
